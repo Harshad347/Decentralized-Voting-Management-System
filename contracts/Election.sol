fecd0b980a321e4df9924899c23fbeb4dfd1f14c
Harshad347 <harshaddukare347@gmail.com>
2022-03-09 17:43:22 +0530

    Smart contract for voter registration done.

---
+++ contracts/Election.sol
@@ -0,0 +1,91 @@
+pragma solidity >=0.4.22 <0.9.0;
+pragma experimental ABIEncoderV2;
+
+contract Election {
+    struct Voter {
+        string name;
+        uint256 voterId;
+        address accountAddress;
+        bool authorized;
+        bool voted;
+        bool exists;
+    }
+
+    mapping(uint256 => Voter) public voters;
+    uint256 public voterCount = 0;
+
+    uint256 public electionState;
+
+    address adminAddress;
+
+    constructor() public {
+        adminAddress = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(
+            msg.sender == adminAddress,
+            "You dont have rights to perform ADMIN operations !"
+        );
+        _;
+    }
+
+    function startElection() public onlyOwner {
+        electionState = 1;
+    }
+
+    function startVotingPhase() public onlyOwner {
+        electionState = 2;
+    }
+
+    function endElection() public onlyOwner {
+        electionState = 0;
+    }
+
+    function registerVoter(string memory _name, uint256 _voterId) public returns (uint256) {
+        require(electionState == 1, "This is not Registration Phase.");
+        address _accountAddress = msg.sender;
+        require(_accountAddress != adminAddress,"You cannot use owner address to register.");
+        require(checkIfAddressExist(_accountAddress),"This Account Address is already registered");
+        require(checkIfVoterIdExist(_voterId),"This Voter Id is already registered.");
+        voterCount++;
+        voters[_voterId] = Voter(
+            _name,
+            _voterId,
+            _accountAddress,
+            false,
+            false,
+            true
+        );
+        emit VoterCreated(_name, _voterId, _accountAddress);
+    }
+
+    function checkIfVoterIdExist(uint256 _inputVoterId) private returns (bool) {
+        for (uint256 i = 1; i <= voterCount; i++) {
+            uint256 idVoter = voters[i].voterId;
+            if (
+                keccak256(abi.encodePacked(idVoter)) == keccak256(abi.encodePacked(_inputVoterId))
+            ) {
+                return false;
+                break;
+            }
+        }
+        return true;
+    }
+
+    function checkIfAddressExist(address _inputAccountAddress) private returns (bool) {
+        for (uint256 i = 1; i <= voterCount; i++) {
+            address addressAccount = voters[i].accountAddress;
+            if (
+                keccak256(abi.encodePacked(addressAccount)) ==
+                keccak256(abi.encodePacked(_inputAccountAddress))
+            ) {
+                return false;
+                break;
+            }
+        }
+        return true;
+    }
+
+    event VoterCreated(string name, uint256 voterId, address accountAddress);
+}
