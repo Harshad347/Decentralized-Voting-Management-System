bcf4425f536df8bce1eef03dcc87cea89fa01a68
Harshad347 <harshaddukare347@gmail.com>
2022-03-09 21:17:37 +0530

    main.js added.

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+function VoterRegForm() {
+  document.getElementById("candidateForm").style.display = "none";
+  document.getElementById("voterForm").style.display = "block";
+  document.getElementById("candidateContent").style.display = "none";
+  document.getElementById("voterContent").style.display = "none";
+  document.getElementById("voteForm").style.display = "none";
+}
